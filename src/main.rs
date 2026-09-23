fc238ff3a12dd2eba973185d93e90e319bcbda5c
Nekon8bit <nekonsantos@gmail.com>
2026-09-22 22:13:14 -0300

    calculator attemp.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    
+    println!("Helamã is Chad!");
+
+    name();
+
+    let name = "murilo dá o canceco".as_bytes();
+
+    let name = String::new();
+}
+
+fn name() {
+    let name = ("Helamã is Chad!");
+
+    println!("{}", name)
+}
